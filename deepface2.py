371160ecf7e6d912b4a9381b725b39bfe64a8f35
ElieDDD <30892423+ElieDDD@users.noreply.github.com>
2025-01-07 16:17:03 +0000

    Update deepface2.py

--- deepface2.py
+++ deepface2.py
@@ -70,7 +70,8 @@ def display_images_with_labels(image_paths, emotions):
 
 # Main Streamlit app
 def main():
-    st.title("decoding through computer vision is a set of decisions about how to interpret visual messages that is shaped by cultural and social values, in addition to producing them, Arnold & Tilton, 2013")
+    st.title("decoding through computer vision is a set of decisions about how to interpret visual messages that is shaped by cultural and social values, in addition to producing them")
+     st.text( "Arnold & Tilton, Distant Viewing 2023")
     st.text("Upload a folder of images, and we'll detect emotions while applying a blur effect to each image!")
 
     # Upload folder
